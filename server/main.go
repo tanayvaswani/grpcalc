3e724c8697e0c8fabb8ea4d2b71beb7ae8045a5e
tanayvaswani <vaswani.tanay9@gmail.com>
2024-10-18 02:56:09 +0530

    feat: grpc service

--- server/main.go
+++ server/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
 
 	"github.com/tanayvaswani/grpcalc/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct {
@@ -20,5 +24,16 @@ func (s *server) Add(
 }
 
 func main() {
+	listner, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to create listener", err)
+	}
 
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pb.RegisterCalculatorServer(grpcServer, &server{})
+	if err := grpcServer.Serve(listner); err != nil {
+		log.Fatalln("Failed to start server", err)
+	}
 }
